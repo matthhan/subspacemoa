4fcb29443b131563b24b3ef9262c9d1d39d0fd44
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2017-02-26 23:39:06 +0100

    remove old unused code

--- src/main/java/moa/r_interface/ClusteringAccessor.java
+++ src/main/java/moa/r_interface/ClusteringAccessor.java
@@ -87,14 +87,4 @@ class ClusteringAccessor {
         }
         return res;
     }
-    /*
-    public static void output2DArray(double[][] arr) {
-        for(int i = 0; i < arr.length;i++) {
-            for(int j=0; j < arr[i].length;j++) {
-                System.out.print(arr[i][j] + ", ");
-            }
-            System.out.println("");
-        }
-    }
-    */
 }
